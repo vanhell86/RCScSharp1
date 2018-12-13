738a64d4bc1de164f1414848c66d7b4f8d16cabe
vanhell86 <45855501+vanhell86@users.noreply.github.com>
2018-12-13 21:27:21 +0200

    Pielikam dažas metodes, un pats basics atkārtots

--- Nodarbiba1_Csharp/Nodarbiba1_Csharp/Program.cs
+++ Nodarbiba1_Csharp/Nodarbiba1_Csharp/Program.cs
@@ -11,18 +11,84 @@ namespace Nodarbiba1_Csharp
         static void Main(string[] args)
         {
             // int, double, String, bool...
-            String name;
-            Console.WriteLine("Sveiki ievadiet savu vārdu: ");
-            name = Console.ReadLine();
-            Console.WriteLine("Sveiki, " + name);
+            // Ctrl + C lai izietu no konsoles.
 
 
+            //PirmaisUzdevums();
+            //Console.WriteLine(Piemers());
+            //int a = Piemers();
+            //Console.WriteLine(Piemers2(3,a));
 
-
+            //Piemers3();
+            Console.WriteLine("Ievadiet pirmo skaitli: ");
+            int sk1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ievadiet otro skaitli: ");
+            int sk2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Rezultats ir: " + AddOrSubstract(sk1, sk2));
 
 
 
             Console.ReadLine();
+        }
+
+        static void PirmaisUzdevums()
+        {
+            Console.WriteLine("Sveiki ievadiet savu vārdu: ");
+            String name = Console.ReadLine();
+            Console.WriteLine("Sveiki, " + name);
+
+        }
+
+        static int Piemers()
+        {
+            return 4 + 3;
+        }
+
+        static int Piemers2(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Piemers3()
+        {
+            Console.WriteLine("Ievadiet simbolus!");
+            String ievade = Console.ReadLine();
+            int a = 5;
+            if (ievade == "vii" && a > 5)        // C# nevajag izmanto .equals, kā tas ir java
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("2");
+            }
+
+            // <
+            // >
+            // <=
+            // >=
+            // ==
+            // !=
+
+            // &&
+            // ||
+
+        }
+
+        static int AddOrSubstract(int a, int b)
+        {
+            String darbiba;
+            Console.WriteLine("Ko vēlaties darīt, saskaitīt vai atņemt?: '+' vai '-' ");
+            darbiba = Console.ReadLine();
+            switch (darbiba)
+            {
+                case "+": return a + b;
+                case "-": return a - b;
+                default: Console.WriteLine("Jūs neievadījāt '+' vai '-' zīmes");
+                break;
+            }
+
+            return 0;
         }
     }
 }
