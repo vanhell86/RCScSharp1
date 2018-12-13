7be08bbbf37014d9bae17894b0463c129f68dd96
vanhell86 <45855501+vanhell86@users.noreply.github.com>
2018-12-13 19:43:30 +0200

    Izveidots projekts un pirmais uzdevums

---
+++ Nodarbiba1_Csharp/Nodarbiba1_Csharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nodarbiba1_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int, double, String, bool...
+            String name;
+            Console.WriteLine("Sveiki ievadiet savu vārdu: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Sveiki, " + name);
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
