f9be113b80773f762e597315a462c7bcd2c4e71e
vanhell86 <45855501+vanhell86@users.noreply.github.com>
2018-12-17 21:28:48 +0200

    MD2 un stradajam ar masiviem un 2d masiviem

---
+++ MajasDarbs2/MajasDarbs2/Calc.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MajasDarbs2
+{
+    class Calc
+    {
+        private double n1;
+        private double n2;
+        private String choice;
+        public Calc (double n1, double n2, String choice)
+        {          
+                this.n1 = n1;
+                this.n2 = n2;
+                this.choice = choice;    
+        }
+
+        public void WhatToDo()
+        {
+            try
+            {
+                switch (choice)
+                {
+                    case "1":
+                    case "+":
+                        {
+                            Console.WriteLine("Skaītļu " + n1 + " un " + n2 + " summa ir: " + Add());
+                            break;
+                        }
+
+                    case "2":
+                    case "-":
+                        {
+                            Console.WriteLine("Skaītļu " + n1 + " un " + n2 + " starpība ir: " + SubStract());
+                            break;
+                        }
+
+                    case "3":
+                    case "*":
+                        {
+                            Console.WriteLine("Skaītļu " + n1 + " un " + n2 + " reizinājums ir: " + MultiPly());
+                            break;
+                        }
+
+                    case "4":
+                    case "/":
+                        {
+                            Console.WriteLine("Skaītļu " + n1 + " un " + n2 + " dalījums ir: " + Divide());
+                            break;
+                        }
+
+                    case "5":
+                    case "^":
+                        {
+                            Console.WriteLine("Skaītlis " + n1 + " , " + n2 + " pakāpē ir: " + Squaring());
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Jūs neievadījāt pareizu darbību!!!");
+                            break;
+                        }
+                }
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Ar nulli nevar dalīt!!!");
+            }
+            //catch(FormatException)
+            //{
+            //    Console.WriteLine("Jūs neievadījāt skaitļus!!!");
+            //}
+        }
+
+        private double Add()
+        {
+            return n1 + n2;
+        }
+
+        private double SubStract()
+        {
+            return n1 - n2;
+        }
+
+        private double MultiPly()
+        {
+            return n1 * n2;
+        }
+
+        private double Divide()
+        {
+            if (n2 == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return n1 / n2;
+        }
+
+
+        private double Squaring()
+        {
+            double result = n1;
+            if (n2 == 0)
+            {
+                return result = 1;
+            }
+            else if (n2 < 0)
+            {
+                for (int i = 1; i < -n2; i++)
+                {
+                    result = result * n1;
+                }
+                return 1 / result;
+            }
+            for (int i = 1; i < n2; i++)
+            {
+                result = result * n1;
+            }
+            return result;
+        }
+
+        private void DivideByZeroException()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MajasDarbs2/MajasDarbs2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace MajasDarbs2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc calc;
+
+            do
+            {
+                Console.Clear();
+                double n1 = GetNum("Ievadiet pirmo skaitli: ");
+                double n2 = GetNum("Ievadiet otro skaitli: ");
+                String choice = GetChoice();
+
+                calc = new Calc(n1, n2, choice);
+                calc.WhatToDo();
+            }
+            while (!Exit());
+
+            //Console.ReadLine();
+        }
+
+        private static double GetNum(String text)
+        {
+        Start:
+            Console.WriteLine(text);
+            try
+            {
+                return Convert.ToDouble(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Jūs neievadījāt skaitli!!!");
+                goto Start;
+            }            
+        }
+
+        private static String GetChoice()
+        {
+            Console.WriteLine("Ko vēlaties darīt? Ievadiet vai nu ciparu vai darbības zīmi.");
+            Console.WriteLine("1. Saskaitīt '+' .");
+            Console.WriteLine("2. Saskaitīt '-' .");
+            Console.WriteLine("3. Reizināt '*' .");
+            Console.WriteLine("4. Dalīt '/' .");
+            Console.WriteLine("5. Kāpināt '^' .");
+            return Console.ReadLine();
+        }
+
+        private static bool Exit()
+        {
+            Console.WriteLine("Ja nevēlaties turpināt nospiediet 'n', ja vēlaties turpināt nospiediet vnk Enter ");
+            String yn = Console.ReadLine();
+            if (yn == "n")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
