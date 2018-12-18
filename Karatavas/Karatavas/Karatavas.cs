35fdd7345bd4d52aac7cb16ccf4d744e1cbe6a04
vanhell86 <45855501+vanhell86@users.noreply.github.com>
2018-12-19 01:48:30 +0200

    Pabeidzu beidzot karatavas

---
+++ Karatavas/Karatavas/Karatavas.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Karatavas
+{
+    class Karatavas
+    {
+        
+
+        public void PrintMas(char[] mas)
+        {
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write(mas[i]);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+
+        public void WriteMas(char[] mas, char[] mas2)
+        {
+            Console.Clear();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas2[i] = '_';
+                Console.Write(mas2[i]);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+
+        public bool CheckForChar(char[] mas, char[] mas2, char burts)
+        {
+            bool check = false;
+                        
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i] == burts)
+                {                   
+                    //Console.WriteLine("Jūs atminējāt vienu burtu! " + mas[i] + " kurs atrodas " + Convert.ToInt16(i + 1) + " pozīcijā");
+                    mas2[i] = burts;
+                    check = true;
+                }               
+            }
+            
+            if (check == false)
+            {
+                Console.WriteLine("Tāds burts nav vārdā");
+                Console.Clear();
+                return check;
+            }
+            
+            Console.Clear();
+            return check;
+        }
+
+        public bool CheckifFilled(char[] mas)
+        {
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i] == '_' )
+                {
+                    return false;
+                }                             
+            }
+            return true;
+            
+        }
+
+    }
+}
---
+++ Karatavas/Karatavas/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+
+namespace Karatavas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ievadiet vārdu, kuru būs jāatmin: ");
+            String name = Console.ReadLine();
+            char[] charMasivs = name.ToCharArray();
+            Karatavas karatavas = new Karatavas();
+            char[] hiddenMas = new char[charMasivs.Length];
+            char burts;
+            int count = 0;
+           
+            karatavas.WriteMas(charMasivs, hiddenMas);
+
+            do
+            {      
+                Console.WriteLine("Atlikušās dzīvības: " + (5 - count));
+                burts = GetChar("Ievadiet burtu. Jums būs 5 iespējas lai atminētu vārdu!");
+                karatavas.CheckForChar(charMasivs, hiddenMas, burts);
+                
+                if (karatavas.CheckifFilled(hiddenMas))
+                {
+                    karatavas.PrintMas(hiddenMas);
+                    Console.WriteLine("Super Jūs uzminējāt vārdu!!!");
+                    break;
+                }
+                if (karatavas.CheckForChar(charMasivs, hiddenMas, burts) == false)
+                {
+                    Console.WriteLine(" Nav tāda burta vārdā ");
+                    count++;
+                }
+                karatavas.PrintMas(hiddenMas);
+
+            } while (count < 5);
+
+            Console.ReadLine();
+        }
+
+        public static char GetChar(String teksts)
+        {
+        Start:
+            Console.WriteLine(teksts);
+            try
+            {
+                String burts = Console.ReadLine();
+                if (Char.IsNumber(burts,0) == true || Char.IsWhiteSpace(burts,0) == true || String.IsNullOrEmpty(burts) == true)
+                {
+                    Console.WriteLine("Jūs ievadījāt skaitli vai tuksumu nevis burtu");
+                    goto Start;
+                }
+                return Convert.ToChar(burts);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Jūs neievadījāt burtu vai ievadījāt vairākus simbolus!!!");
+                goto Start;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Jūs neko neievadījāt");
+                goto Start;
+            }
+        }
+    }
+}
